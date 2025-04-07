27c96c672a171023145298c1adeddb379355e12b
pwwddxx <phuwadonz71@gmail.com>
2025-04-07 23:45:13 +0700

    fix repo

---
+++ Codeforces/unofficial/oneForAll.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#include <cstdio>
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define pb push_back
+#define all(c) (c).begin(), (c).end()
+#define sz(x) (int)(x).size()
+#define inArray(ar) for (auto &x : (ar)) cin >> x;
+#define printAr(ar) for (auto &x : (ar)) { cout << x << " "; cout << '\n' }
+#define inMatrix(ar, rows, cols) for (int i = 0; i < rows; ++i) for (int j = 0; j < cols; ++j) cin >> ar[i][j];
+#define Pi atan(1) * 4
+
+void runcase() {
+3
+}
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int t(1); //cin >> t;
+  while (t--) runcase();
+  return 0;
+}
+
